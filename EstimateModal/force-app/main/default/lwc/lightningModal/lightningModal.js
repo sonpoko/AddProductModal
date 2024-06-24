a11b245788f778854968dd17f4e7d8e72a6077f1
sonpoko <snk.fuo@gmail.com>
2024-06-24 22:23:50 +0900

    hey

---
+++ EstimateModal/force-app/main/default/lwc/lightningModal/lightningModal.js
@@ -0,0 +1,20 @@
+import { LightningElement, track } from 'lwc';
+
+export default class ModalWithSpinner extends LightningElement {
+    @track isModalOpen = false;
+    @track isLoading = false;
+
+    openModal() {
+        this.isModalOpen = true;
+        this.isLoading = true;
+        
+        // Simulate loading process
+        setTimeout(() => {
+            this.isLoading = false;
+        }, 2000); // Adjust the timeout as needed
+    }
+
+    closeModal() {
+        this.isModalOpen = false;
+    }
+}
